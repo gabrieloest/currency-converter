78f7a1f396bd665f7e2364e81ae52e31a99b9176
Gabriel Oest <gabriel.oest@esig.com.br>
2018-09-03 14:51:06 -0300

    Add test h2 configuration

--- src/test/java/com/zooplus/currencyconverter/CurrencyConverterApplicationTests.java
+++ src/test/java/com/zooplus/currencyconverter/CurrencyConverterApplicationTests.java
@@ -2,11 +2,14 @@ package com.zooplus.currencyconverter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.zooplus.currencyconverter.configuration.DevDatasourceConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { DevDatasourceConfig.class }, loader = AnnotationConfigContextLoader.class)
 public class CurrencyConverterApplicationTests {
 
 	@Test
