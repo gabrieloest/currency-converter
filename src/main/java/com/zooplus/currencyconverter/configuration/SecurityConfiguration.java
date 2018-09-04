44ba48a85ac31260d6ec120c922c8f5f394f1763
Gabriel Oest <gabriel.oest@esig.com.br>
2018-09-04 11:36:21 -0300

    Add actuator

--- src/main/java/com/zooplus/currencyconverter/configuration/SecurityConfiguration.java
+++ src/main/java/com/zooplus/currencyconverter/configuration/SecurityConfiguration.java
@@ -26,6 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers(
+						"/actuator/health/**",
 						"/console/**",
 						"/registration/**",
 						"/js/**",
